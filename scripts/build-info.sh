fddd1a1d729bfa2a81e498d41616b1ad6015ddc8
Kyle Terry <kyle@kyleterry.com>
2019-07-09 16:57:01 -0700

    adds ci scripts and gcloud configuration for docker pushes

---
+++ scripts/build-info.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export RELEASE_MANIFEST=ci-release-manifest
+export TRAVIS_BRANCH="${TRAVIS_BRANCH:-$(git branch | grep \* | cut -d ' ' -f2)}"
+DIRTY=
+[ -n "$(git status --porcelain --untracked-files=no)" ] && DIRTY="-dirty"
+TAG=$(git tag -l --contains HEAD | head -n 1)
+if [ -n "${TAG}" ]; then
+        export VERSION="${TAG}${DIRTY}"
+    else
+        export VERSION="$(git rev-parse --short HEAD)${DIRTY}"
+fi
+
+fail() {
+    echo "ERROR: ${1}"
+    exit 1
+}
