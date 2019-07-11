f0cb791721d22a560a01ee01007869fc036e62dd
Brian Maher <brian.maher@puppet.com>
2019-07-11 13:02:06 -0700

    Modify nebula-slack-notification so it obtains the spec from the $SPEC_URL

--- cmd/nebula-slack-notification/main.go
+++ cmd/nebula-slack-notification/main.go
@@ -2,38 +2,70 @@ package main
 
 import (
 	"fmt"
-	"flag"
 	"github.com/puppetlabs/nebula-tasks/pkg/notify/slack"
 	"os"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"errors"
 )
 
+type Spec struct {
+	Apitoken string `json:"apitoken"`
+	Channel  string `json:"channel"`
+	Message  string `json:"message"`
+	Username string `json:"username"`
+}
+
+func getSpec() (*Spec, error) {
+	specUrl := os.Getenv("SPEC_URL")
+	if "" == specUrl {
+		return nil, errors.New("Missing required environment variable: SPEC_URL")
+	}
+	resp, err := http.Get(specUrl)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("GET %s: %v", specUrl, err))
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("GET %s: %v", specUrl, err))
+	}
+	if resp.StatusCode / 100 != 2 {
+		return nil, errors.New(fmt.Sprintf("GET %s -> %d: %v", specUrl, resp.StatusCode, err))
+	}
+	var spec Spec
+	err = json.Unmarshal([]byte(body), &spec)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("GET %s -> %s: %v", specUrl, body, err))
+	}
+	return &spec, nil
+}
 
 func main() {
-	var channel = flag.String("channel", "", "The slack channel to notify")
-	var message = flag.String("message", "", "The message to notify the channel with")
-	var username = flag.String("username", "Nebula", "The username to publish the message as")
+	spec, err := getSpec()
+	if nil != err {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	if "" == spec.Channel || "" == spec.Message || "" == spec.Apitoken {
+		fmt.Printf("Missing required fields. Expect spec to contain 'apitoken', 'channel' and 'message', got %v\n", *spec)
+		os.Exit(1)
+	}
+	if "" == spec.Username {
+		spec.Username = "Nebula"
+	}
 
-	flag.Parse()
-	if "" == *channel || "" == *message {
-		fmt.Println("Missing required flag(s)")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-	token := os.Getenv("SLACK_TOKEN")
-	if "" == token {
-		fmt.Println("Missing required environment variable: SLACK_TOKEN")
-		os.Exit(1)
-	}
-	client := slack.New(token)
+	client := slack.New(spec.Apitoken)
 	res, err := client.Chat().PostMessage(
 		slack.PostMessageRequest{
-			Channel:  *channel,
-			Text:     *message,
-			Username: *username,
+			Channel:  spec.Channel,
+			Text:     spec.Message,
+			Username: spec.Username,
 		})
 
 	if nil != err {
-		fmt.Println(err.Error())
+		fmt.Printf("%v\n", err)
 		os.Exit(1)
 	}
 	fmt.Printf("Response: %+v\n", res)
