9b13de200d9170d1808a1a77dd2cb8472a7c739e
Kyle Terry <kyle@kyleterry.com>
2019-07-18 13:35:29 -0700

    adds step back to travis config and enables build-info to work locally

--- scripts/build-info.sh
+++ scripts/build-info.sh
@@ -2,6 +2,8 @@
 
 export RELEASE_MANIFEST=ci-release-manifest
 export TRAVIS_BRANCH="${TRAVIS_BRANCH:-$(git branch | grep \* | cut -d ' ' -f2)}"
+export TRAVIS_EVENT_TYPE="${TRAVIS_EVENT_TYPE:-}"
+
 DIRTY=
 [ -n "$(git status --porcelain --untracked-files=no)" ] && DIRTY="-dirty"
 TAG=$(git tag -l --contains HEAD | head -n 1)
