82784922b791d793e1ef8b0622ee93a9f9e79464
Kyle Terry <kyle@kyleterry.com>
2019-09-09 17:06:49 -0700

    annotating the metadata type

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -17,6 +17,7 @@ func NewTaskInterface(opts taskutil.DefaultPlanOptions) *TaskInterface {
 	return &TaskInterface{opts}
 }
 
+// Metadata represents task metadata (such as the name of the task and ID).
 type Metadata struct {
 	ID   string
 	Name string
