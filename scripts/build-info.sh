9af9111951b2a4cb285a996833439657f0432bf9
Hunter Haugen <hunter@haugens.org>
2019-10-01 17:21:13 -0700

    Push containers for :latest usage via master branch
    Latest isn't a real tag; it just means the latest container without a
    tag.
    
    Travis doesn't have a way of exposing the "build branch" to the
    environment (see https://github.com/travis-ci/travis-ci/issues/6652) so
    the build scripts can't detect a merge to master.
    
    Travis also doesn't have a way of doing "conditional environment variables"
    but does have conditional stages based on the branch. But for pull
    requests this is the "base branch." We don't want to release latest from
    pull request builds.
    
    Travis does expose an environment variable indicating whether it is a PR
    build or a merge build, as per https://docs.travis-ci.com/user/pull-requests/

--- scripts/build-info.sh
+++ scripts/build-info.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 export TRAVIS_BRANCH="${TRAVIS_BRANCH:-$(git branch | grep \* | cut -d ' ' -f2)}"
+export TRAVIS_PULL_REQUEST="${TRAVIS_PULL_REQUEST:-}"
 export TRAVIS_EVENT_TYPE="${TRAVIS_EVENT_TYPE:-}"
 export NEBULA_TASKS_BUILD_DIR="${NEBULA_TASKS_BUILD_DIR:-.build}"
 export NEBULA_TASKS_RELEASE_MANIFEST="${NEBULA_TASKS_RELEASE_MANIFEST:-${NEBULA_TASKS_BUILD_DIR}/ci-release-manifest}"
+
+export NEBULA_TASKS_RELEASE_LATEST=
+[ "${TRAVIS_BRANCH}" = "master" ] && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && export NEBULA_TASKS_RELEASE_LATEST=true
 
 DIRTY=
 [ -n "$(git status --porcelain --untracked-files=no)" ] && DIRTY="-dirty"
