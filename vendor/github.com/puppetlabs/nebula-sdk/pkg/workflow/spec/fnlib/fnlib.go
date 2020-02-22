388e2cc2e46162589b14f8e35fd12a77d6276a18
Kyle Terry <kyle@kyleterry.com>
2020-02-21 17:28:52 -0800

    vendoring

--- vendor/github.com/puppetlabs/nebula-sdk/pkg/workflow/spec/fnlib/fnlib.go
+++ vendor/github.com/puppetlabs/nebula-sdk/pkg/workflow/spec/fnlib/fnlib.go
@@ -6,11 +6,15 @@ var (
 	library = map[string]fn.Descriptor{
 		"append":        appendDescriptor,
 		"concat":        concatDescriptor,
+		"equals":        equalsDescriptor,
 		"jsonUnmarshal": jsonUnmarshalDescriptor,
 		"merge":         mergeDescriptor,
+		"notEquals":     notEqualsDescriptor,
 	}
 )
 
+// Library creates an fn.Map of all the core functions supported
+// by the platform.
 func Library() fn.Map {
 	return fn.NewMap(library)
 }
