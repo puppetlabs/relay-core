2bc2c4d1df180f56624e55ffadd7a84fb634f028
Noah Fontes <noah.fontes@puppet.com>
2020-04-10 17:23:03 -0700

    Revert "Temporary commit to deploy to feature cluster"
    This reverts commit 44026b1755c04896fe79a5ffd6178fccbd92ce2d.

--- nebula-deploy.sh
+++ nebula-deploy.sh
@@ -6,4 +6,3 @@
 NEBULA_WORKFLOWS[improvements/workflows]=nebula
 NEBULA_WORKFLOWS[improvements/cluster-security]=nebula-system-deploy-tr-security
 NEBULA_WORKFLOWS[features/PN-151-prometheus-instrumentation]=features-tr-metrics
-NEBULA_WORKFLOWS[tasks/pn-576-triggers-crds]=relay-feature-triggers-v2
