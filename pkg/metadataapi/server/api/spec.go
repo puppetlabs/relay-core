250d90f629cf2b6ff025245d725117f104f27ae3
Kyle Terry <kyle@kyleterry.com>
2020-10-12 15:27:24 -0700

    removes schema validation from the metadata api spec endpoint

--- pkg/metadataapi/server/api/spec.go
+++ pkg/metadataapi/server/api/spec.go
@@ -1,17 +1,13 @@
 package api
 
 import (
-	goerrors "errors"
 	"net/http"
 
 	utilapi "github.com/puppetlabs/horsehead/v2/httputil/api"
-	"github.com/puppetlabs/horsehead/v2/instrumentation/alerts/trackers"
 	"github.com/puppetlabs/relay-core/pkg/expr/evaluate"
 	"github.com/puppetlabs/relay-core/pkg/manager/resolve"
 	"github.com/puppetlabs/relay-core/pkg/metadataapi/errors"
 	"github.com/puppetlabs/relay-core/pkg/metadataapi/server/middleware"
-	"github.com/puppetlabs/relay-core/pkg/util/image"
-	wfspec "github.com/puppetlabs/relay-core/pkg/workflow/spec"
 )
 
 func (s *Server) GetSpec(w http.ResponseWriter, r *http.Request) {
@@ -56,48 +52,5 @@ func (s *Server) GetSpec(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	env := evaluate.NewJSONResultEnvelope(rv)
-
-	if s.specSchemaRegistry != nil && env.Complete {
-		sm, err := managers.StepMetadata().Get(ctx)
-		if err != nil {
-			utilapi.WriteError(ctx, w, ModelReadError(err))
-			return
-		}
-
-		ref, err := image.RepoReference(sm.Image)
-		if err != nil {
-			utilapi.WriteError(ctx, w, errors.NewStepImageParseError().WithCause(err))
-			return
-		}
-
-		repository := ref.Context()
-
-		capture, ok := trackers.CapturerFromContext(ctx)
-		if ok {
-			repo := repository.RepositoryStr()
-			capture = capture.WithTags(trackers.Tag{Key: "relay.spec.validation-error", Value: repo})
-
-			var captureErr error
-
-			schema, err := s.specSchemaRegistry.GetByStepRepository(repo)
-			if err != nil {
-				captureErr = err
-				if !goerrors.Is(err, &wfspec.SchemaDoesNotExistError{}) {
-					captureErr = errors.NewSpecSchemaLookupError().WithCause(err)
-				}
-			} else {
-				if err := schema.ValidateGo(env.Value.Data); err != nil {
-					captureErr = errors.NewSpecSchemaValidationError().WithCause(err)
-				}
-			}
-
-			if captureErr != nil {
-				report := capture.Capture(captureErr)
-				report.Report(ctx)
-			}
-		}
-	}
-
-	utilapi.WriteObjectOK(ctx, w, env)
+	utilapi.WriteObjectOK(ctx, w, evaluate.NewJSONResultEnvelope(rv))
 }
