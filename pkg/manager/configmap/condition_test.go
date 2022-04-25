e514d535dbd5dea8f81b90550fafaf5c4f766689
Rick Lane <rick.lane@puppet.com>
2022-04-25 11:32:53 -0700

    Handle conditions not defined (validly)

--- pkg/manager/configmap/condition_test.go
+++ pkg/manager/configmap/condition_test.go
@@ -4,26 +4,97 @@ import (
 	"context"
 	"testing"
 
+	"github.com/puppetlabs/relay-core/pkg/expr/parse"
 	"github.com/puppetlabs/relay-core/pkg/manager/configmap"
 	"github.com/puppetlabs/relay-core/pkg/model"
 	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 )
 
-func TestConditionManager(t *testing.T) {
+func TestConditionManagerWhenConditionsAreDefined(t *testing.T) {
 	ctx := context.Background()
-	step := &model.Step{
-		Run:  model.Run{ID: "foo"},
-		Name: "bar",
+
+	tests := []struct {
+		Name                        string
+		Action                      model.Action
+		Value                       interface{}
+		ExpectedConditionExpression parse.Tree
+	}{
+		{
+			Name: "Valid step and value",
+			Action: &model.Step{
+				Run:  model.Run{ID: "foo"},
+				Name: "bar",
+			},
+			Value:                       true,
+			ExpectedConditionExpression: true,
+		},
+		{
+			Name: "Valid step and nil value",
+			Action: &model.Step{
+				Run:  model.Run{ID: "foo"},
+				Name: "bar",
+			},
+			Value: nil,
+		},
 	}
 
-	cm := configmap.NewConditionManager(step, configmap.NewLocalConfigMap(&corev1.ConfigMap{}))
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			cm := configmap.NewConditionManager(test.Action, configmap.NewLocalConfigMap(&corev1.ConfigMap{}))
 
-	cond, err := cm.Set(ctx, true)
-	require.NoError(t, err)
-	require.Equal(t, true, cond.Tree)
+			condition, err := cm.Set(ctx, test.Value)
+			require.NoError(t, err)
+			require.NotNil(t, condition)
+			require.Equal(t, test.ExpectedConditionExpression, condition.Tree)
 
-	cond, err = cm.Get(ctx)
-	require.NoError(t, err)
-	require.Equal(t, true, cond.Tree)
+			condition, err = cm.Get(ctx)
+
+			require.NoError(t, err)
+			require.NotNil(t, condition)
+			require.Equal(t, test.ExpectedConditionExpression, condition.Tree)
+		})
+	}
+}
+
+func TestConditionManagerWhenNoConditionsAreDefined(t *testing.T) {
+	ctx := context.Background()
+
+	tests := []struct {
+		Name          string
+		Action        model.Action
+		ExpectedError error
+	}{
+		{
+			Name: "Valid step and no value set",
+			Action: &model.Step{
+				Run:  model.Run{ID: "foo"},
+				Name: "bar",
+			},
+			ExpectedError: model.ErrNotFound,
+		},
+		{
+			Name: "Valid trigger and no value set",
+			Action: &model.Trigger{
+				Name: "bar",
+			},
+			ExpectedError: model.ErrNotFound,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Name, func(t *testing.T) {
+			cm := configmap.NewConditionManager(test.Action, configmap.NewLocalConfigMap(&corev1.ConfigMap{}))
+
+			condition, err := cm.Get(ctx)
+
+			if test.ExpectedError != nil {
+				require.Equal(t, test.ExpectedError, err)
+				require.Nil(t, condition)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, condition)
+			}
+		})
+	}
 }
